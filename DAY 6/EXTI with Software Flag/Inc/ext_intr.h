288faf924f9271d551e4ded36e98930dc6a08015
ashutoshbhosale2005 <ashutoshbhosale2005@gmail.com>
2025-12-22 08:58:30 +0530

    DAY 5-6

---
+++ DAY 6/EXTI with Software Flag/Inc/ext_intr.h
@@ -0,0 +1,14 @@
+/*
+ * ext_intr.h
+ *
+ *  Created on: Dec 18, 2025
+ *      Author: kiran_z6dopa8
+ */
+
+#ifndef EXT_INTR_H_
+#define EXT_INTR_H_
+extern volatile int exti0_flag;
+
+void exti0_IRQHandler(void);
+
+#endif /* EXT_INTR_H_ */
